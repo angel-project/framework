0be2bcb9217f4de9a7c83e06d776aa9d687fff48
MadaoIsMyBrother <34904199+MadaoIsMyBrother@users.noreply.github.com>
2018-04-24 17:15:23 -0400

    Add files via upload

--- build/welcome.php
+++ build/welcome.php
@@ -6,3 +6,30 @@
       'url' => user::url()
     ]);
   }); //example page
+
+  build::get('test',function(){
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+      $path = getcwd();
+      $path = $path.'\vendor\angel-project\core\src';
+      require $path.'\ary\test.php';
+      require $path.'\build\test.php';
+      require $path.'\curl\test.php';
+      require $path.'\is\test.php';
+      require $path.'\js\test.php';
+      require $path.'\jump\test.php';
+      require $path.'\str\test.php';
+      require $path.'\user\test.php';
+     }
+    else {
+      $path = getcwd();
+      $path = $path.'/vendor/angel-project/core/src';
+      require $path.'/ary/test.php';
+      require $path.'/build/test.php';
+      require $path.'/curl/test.php';
+      require $path.'/is/test.php';
+      require $path.'/js/test.php';
+      require $path.'/jump/test.php';
+      require $path.'/str/test.php';
+      require $path.'/user/test.php';
+    }
+  }); //example page
